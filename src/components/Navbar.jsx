0126732f726a9f0bb017f377de8b8ab47a10232d
360manish <manishmishra786876@gmail.com>
2023-11-18 15:11:30 +0530

    modified button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar(props){
         <div className="flex justify-between item-center">
             <div className="mt-5 ml-5 mr-5 text-white font-bold text-5xl">Comic Generator</div>
             <div className="mt-5 ml-5 mr-5">
-            <Button className="text-white" variant="outlined" onClick={props.onClick}>
-                Create Comic
-            </Button>
+            <button className="text-white border-red border-2 bg-red-800 rounded" variant="outlined" onClick={props.onClick}>
+                <div className="text-white font-bold text-xl px-2 py-2">Create Comic</div>
+            </button>
             </div>
         </div>
     )
